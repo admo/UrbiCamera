1199aff0dc6996f5d51db3b102be7b1f16344d2d
Adam Oleksy <adam.oleksy@gmail.com>
2011-11-22 16:02:32 +0100

    Poprawki typow

--- src/urbicolordetector.cpp
+++ src/urbicolordetector.cpp
@@ -33,7 +33,7 @@ private:
     void setColor(int, int, int, int, int, int); // change color
     void SetColor(int, int, int, int, int, int); // change color
     void detectFrom(UImage); // image processing function
-    void SetImage(UImage&);
+    void SetImage(UImage);
 
     // Temporary variables for image processing function
     Mat mResultImage;
@@ -206,8 +206,70 @@ void UColorDetector::detectFrom(UImage src) {
     image = mBinImage;
 }
 
-void UColorDetector::SetImage(UImage& var) {
-    detectFrom(var);
+void UColorDetector::SetImage(UImage src) {
+    // Build MatImage with data from uImage
+    Mat processImage(Size(src.width, src.height), CV_8UC3, src.data);
+
+    // Resize image
+    Mat resizedImage(cvRound(processImage.rows/scale.as<double>()), cvRound(processImage.cols/scale.as<double>()), CV_8UC1);
+    resize(processImage, resizedImage, resizedImage.size(), 0, 0, INTER_LINEAR);
+    width = resizedImage.cols;
+    height = resizedImage.rows;
+    
+    // Copy image to mMatImage as grayscaled image
+    Mat grayscaleImage;
+    cvtColor(resizedImage, grayscaleImage, CV_RGB2GRAY);
+    cvtColor(grayscaleImage, mResultImage, CV_GRAY2RGB);
+
+    // Compute fps - algorithm efficency
+    int64 startTick = getTickCount();
+    fps = static_cast<double>(getTickFrequency()) / (startTick - mLastTick);
+    mLastTick = startTick;
+
+    // Convert From RGB to HSV color space
+    Mat hsvImage;
+    cvtColor(resizedImage, hsvImage, CV_RGB2HSV);
+
+    // Find regions
+    Mat thresholdImage;
+    inRange(hsvImage, hsv_min, hsv_max, thresholdImage);
+
+    // Filter
+    medianBlur(thresholdImage, thresholdImage, 13);
+
+    // Add detected region to gray scale image
+    add(mResultImage, resizedImage, mResultImage, thresholdImage);
+
+    // Compute center of the position 
+    cv::Moments computedMoments(moments(thresholdImage));
+    int xx = static_cast<int>(computedMoments.m10 / computedMoments.m00);
+    int yy = static_cast<int>(computedMoments.m01 / computedMoments.m00);
+    
+    // Finally set visible
+    if ((xx > 0) && (yy > 0)) {
+        // Set point and visible
+        x = xx - processImage.cols / 2;
+        y = -yy + processImage.rows / 2;
+        visible = 1;
+
+        // Draw line from image center to object center
+        line(mResultImage, Point(xx, yy), Point(mResultImage.cols/2, mResultImage.rows/2), Scalar(255, 0, 0), 2);
+    } else {
+        x = 0;
+        y = 0;
+        visible = 0;
+    }
+
+    // Draw horizontal and vertical line in the middle of the image
+    line(mResultImage, Point(0, mResultImage.rows/2), Point(mResultImage.cols, mResultImage.rows/2), Scalar(100, 100, 100), 1);
+    line(mResultImage, Point(mResultImage.cols/2, 0), Point(mResultImage.cols/2, mResultImage.rows), Scalar(100, 100, 100), 1);
+    
+    // Copy mResult image to UImage
+    mBinImage.image.width = mResultImage.cols;
+    mBinImage.image.height = mResultImage.rows;
+    mBinImage.image.size = mResultImage.cols * mResultImage.rows * 3;
+    mBinImage.image.data = mResultImage.data;
+    image = mBinImage;
 }
 
 UStart(UColorDetector);
--- src/urbiobjectdetector.cpp
+++ src/urbiobjectdetector.cpp
@@ -38,7 +38,7 @@ private:
     void changeHaarCascade();
     void changeScale(UVar&);
     void detectFrom(UImage); // image processing function
-    void SetImage(UImage&);
+    void SetImage(UImage);
     
     // Urbi variables
     // Results
@@ -196,8 +196,66 @@ void UObjectDetector::detectFrom(UImage src) {
     image = mBinImage;
 }
 
-void UObjectDetector::SetImage(UImage& var) {
-    detectFrom(var);
+void UObjectDetector::SetImage(UImage src) {
+    // Build MatImage with data from uImage
+    Mat processImage(Size(uImage.width, uImage.height), CV_8UC3, uImage.data);
+    
+    // Resize image
+    Mat smallImage(cvRound(processImage.rows/scale.as<double>()), cvRound(processImage.cols/scale.as<double>()), CV_8UC1);
+    resize(processImage, mResultImage, smallImage.size(), 0, 0, INTER_LINEAR);
+    width = mResultImage.cols;
+    height = mResultImage.rows;
+    
+    cvtColor(mResultImage, smallImage, CV_RGB2GRAY);
+    equalizeHist(smallImage, smallImage);
+    
+    if(mCVCascade.empty()) {
+        throw std::runtime_error("Cascade classifier not loaded");
+    } else {
+        // ...to measure all processing time
+        int64 startTick = getTickCount();
+        fps = static_cast<double>(getTickFrequency()) / (startTick - mLastTick);
+        mLastTick = startTick;
+        
+        vector<Rect> objects;
+        mCVCascade.detectMultiScale(smallImage, objects, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+        
+        if(!objects.empty()) {
+            //TODO wykorzystać boost??
+            vector<Rect>::const_iterator biggest = objects.begin();
+            for(vector<Rect>::const_iterator i = objects.begin(); i < objects.end(); ++i) {
+                if(i->area() > biggest->area())
+                    biggest = i;
+            }
+            
+            // Draw on a mResultImage
+            Point center(biggest->x+biggest->width/2, biggest->y+biggest->height/2);
+            int radius = (biggest->height + biggest->height)/4;
+            circle(mResultImage, center, radius, Scalar(255,0,0), 3, 8, 0);
+            line(mResultImage, center, Point(mResultImage.cols/2, mResultImage.rows/2), Scalar(255, 0, 0), 2);
+            
+            // Set position of the object
+            x = biggest->x-mResultImage.cols/2;
+            y = -biggest->y-mResultImage.rows/2;
+            
+            visible = 1;
+        } else {
+            visible = 0;
+            x = 0;
+            y = 0;
+        }
+    }
+    
+    // Draw horizontal and vertical line in the middle of the image
+    line(mResultImage, Point(0, mResultImage.rows/2), Point(mResultImage.cols, mResultImage.rows/2), Scalar(100, 100, 100), 1);
+    line(mResultImage, Point(mResultImage.cols/2, 0), Point(mResultImage.cols/2, mResultImage.rows), Scalar(100, 100, 100), 1);
+    
+    // Copy mResult image to UImage
+    mBinImage.image.width = mResultImage.cols;
+    mBinImage.image.height = mResultImage.rows;
+    mBinImage.image.size = mResultImage.cols * mResultImage.rows * 3;
+    mBinImage.image.data = mResultImage.data;
+    image = mBinImage;
 }
 
 UStart(UObjectDetector);
